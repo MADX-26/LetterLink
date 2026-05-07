b47e7626686888796fe9eea8cc49cdbe38936e1f
k madhan <mgmngamerz@gmail.com>
2026-05-07 16:34:10 +0530

    updated dictonary

--- wordgame/config/settings.py
+++ wordgame/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$#xipai4efowo-=chy!yje9(+0u+ot=4mz21^m=g31tx*%-@gc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
