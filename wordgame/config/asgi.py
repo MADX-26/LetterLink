17a48fa9e262de3b5f26f7569049115811689f98
MADX-26 <nsmg1026@gmail.com>
2026-05-10 02:17:21 +0530

    Fixed asgi issue

--- wordgame/config/asgi.py
+++ wordgame/config/asgi.py
@@ -1,17 +1,22 @@
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-
-import game.routing
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+from django.core.asgi import get_asgi_application
 
 django_asgi_app = get_asgi_application()
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+import game.routing
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": URLRouter(
-        game.routing.websocket_urlpatterns
+
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            game.routing.websocket_urlpatterns
+        )
     ),
 })
