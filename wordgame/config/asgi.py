946dc08a1880725a154c5c57be14bd397713b61d
MADX-26 <nsmg1026@gmail.com>
2026-05-10 02:11:46 +0530

    Fixed Daphne setup

--- wordgame/config/asgi.py
+++ wordgame/config/asgi.py
@@ -1,46 +1,17 @@
 import os
 
-from django.core.asgi import (
-    get_asgi_application
-)
-
-from channels.routing import (
-    ProtocolTypeRouter,
-    URLRouter
-)
-
-from channels.auth import (
-    AuthMiddlewareStack
-)
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 
 import game.routing
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-
-os.environ.setdefault(
-
-    'DJANGO_SETTINGS_MODULE',
-
-    'config.settings'
-)
-
-
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-
-    "http":
-
-        get_asgi_application(),
-
-
-    "websocket":
-
-        AuthMiddlewareStack(
-
-            URLRouter(
-
-                game.routing
-                .websocket_urlpatterns
-            )
-        ),
+    "http": django_asgi_app,
+    "websocket": URLRouter(
+        game.routing.websocket_urlpatterns
+    ),
 })
